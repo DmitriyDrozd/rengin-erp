a750ccc3857f766f29281a6ad48aee72fd150f6b
Max Shammasov <miramaxis@gmail.com>
2023-07-29 04:33:34 +0300

    ag-grid added

--- packages/front/src/components/pages/addresses/AddressesListPage.tsx
+++ packages/front/src/components/pages/addresses/AddressesListPage.tsx
@@ -42,7 +42,41 @@ export default () => {
             <Breadcrumb style={{ margin: '16px 0' }}>
                 <Breadcrumb.Item>Адреса</Breadcrumb.Item>
             </Breadcrumb>
-            <RCRUDTable itemNavBase={'/app/in/addresses'}  dataSource={list} columns={columns} name={'адреса'} />
+            {
+                //<RCRUDTable itemNavBase={'/app/in/addresses'}  dataSource={list} columns={columns} name={'адреса'} />
+            }
+            <AddressesTable/>
         </InnerPageBase>
     )
 }
+
+
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+const AddressesTable = () => {
+    const [rowData] = useState([
+        {make: "Toyota", model: "Celica", price: 35000},
+        {make: "Ford", model: "Mondeo", price: 32000},
+        {make: "Porsche", model: "Boxster", price: 72000}
+    ]);
+
+    const [columnDefs] = useState([
+        { field: 'make' },
+        { field: 'model' },
+        { field: 'price' }
+    ]);
+
+    return (
+        <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={columnDefs}>
+            </AgGridReact>
+        </div>
+    );
+};
