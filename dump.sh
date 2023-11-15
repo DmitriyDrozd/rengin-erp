9439f2963b0ff82b8844aca3048de035691a2813
root <root@rengindesk.ru>
2023-11-15 21:06:10 +0300

    wow

--- dump.sh
+++ dump.sh
@@ -4,7 +4,7 @@ DEST=/dumps/rengin/$DIR
 mkdir -p $DEST
 FILE=$DEST/archive.gz
 echo $FILE
-mongodump -h localhost:27017 -d rengin --archive=$FILE
+mongodump -h localhost:27017 -d rengin -u root -p BuildMeUp --authenticationDatabase admin --archive=$FILE
 rm -rf  /dumps/rengin/last
 
 cp -r $DEST /dumps/rengin/last
--- packages/service/src/getServiceEnv.ts
+++ packages/service/src/getServiceEnv.ts
@@ -1,4 +1,4 @@
 export default () => ({
     RENGIN_SERVICE_PORT: process.env.RENGIN_SERVICE_PORT || 9380,
-    RENGIN_SERVICE_MONGO_URI: process.env.RENGIN_SERVICE_MONGO_URI || 'mongodb://localhost:27017/rengin',
+    RENGIN_SERVICE_MONGO_URI: process.env.RENGIN_SERVICE_MONGO_URI || 'mongodb://127.0.0.1:27017/rengin',
 })
