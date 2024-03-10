37a58f437aa9b28a672dda5826a94ec2e3820a2b
dzmitryi.drozd <dzmitryi.drozd@gmail.com>
2024-03-10 17:53:38 +0300

    Исправлен deprecated метод

--- packages/service/src/rest/serverSaga.ts
+++ packages/service/src/rest/serverSaga.ts
@@ -1,14 +1,13 @@
-import {all, call, fork, put, select, setContext, take} from 'typed-redux-saga'
+import {call, fork, put, select, setContext} from 'typed-redux-saga'
 
 import {SagaOptions} from '../sagaOptions'
 import startServer from './startServer';
-import {bootstrapCruds} from 'iso/src/store/bootstrapDuck';
 import {defaultAdminUser, usersResource} from 'iso/src/store/bootstrap/repos/users'
 import {bootstrapRepositories} from "../repositories/bootstrapRepositories";
 import {config} from "@app-config/main";
 
 var current = global.performance.now()
-const timeLog = (title: string, ...args?: any[]) => {
+const timeLog = (title: string, ...args: any) => {
     const now = global.performance.now()
     console.log(title, now-current, ...args)
     current = now
@@ -19,9 +18,9 @@ export function* serverSaga(io: SagaOptions) {
     yield* setContext({io})
     yield* fork(bootstrapRepositories, io)
     timeLog('Repositories bootstraped')
-    const effects = yield* all(
-        bootstrapCruds.map(crud => take(crud.actions.reset.isType))
-    )
+    // const effects = yield* all(
+    //     bootstrapCruds.map(crud => take(crud.actions.reset.isType))
+    // )
     timeLog('cruds reset')
     const users = yield* select(usersResource.selectAll)
     if(users.length === 0) {
@@ -41,7 +40,7 @@ timeLog('fastify startServer completed')
 
             const host = '0.0.0.0'
             console.log('SERVICE listen to ', host, config.PORT)
-            await fastify.listen(config.PORT, '0.0.0.0')
+            await fastify.listen({ port: config.PORT, host: '0.0.0.0' })
             // fastify.blipp()
             fastify.printRoutes({commonPrefix: false})
             console.info(`server listening on `, config.PORT)
