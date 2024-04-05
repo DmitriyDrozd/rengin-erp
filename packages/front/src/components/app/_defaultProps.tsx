7f258d9dd8f83a3e2220c50f9db81417fba84805
dzmitryi.drozd <dzmitryi.drozd@gmail.com>
2024-04-05 17:36:05 +0300

    переделано ограничение админа

--- packages/front/src/components/app/_defaultProps.tsx
+++ packages/front/src/components/app/_defaultProps.tsx
@@ -1,79 +1,95 @@
 import * as Icons from '@ant-design/icons';
-import {AppstoreOutlined, BarChartOutlined, CalendarOutlined, ImportOutlined, MailOutlined} from '@ant-design/icons';
+import {AppstoreOutlined, BarChartOutlined, CalendarOutlined, MailOutlined} from '@ant-design/icons';
 import React from 'react'
 import {RoleType} from "iso/src/store/bootstrap/repos/users"
 
+type TRoute = {
+    path: string,
+    name: string,
+    icon?: JSX.Element,
+    admin?: boolean,
+    routes?: TRoute[],
+};
+
+const filterAdminRole = (isAdmin: boolean) => (item: TRoute) => item.admin ? isAdmin : true;
+const mapFiltered = (item: TRoute) => {
+    const { admin, ...rest } = item;
+
+    return rest;
+};
+
 export default (role: RoleType) => {
     const isAdmin = role === 'руководитель';
-    const adminRoutesInit = isAdmin ?
-        [{
+    const adminFilter = filterAdminRole(isAdmin);
+
+    const routesDictionaries: TRoute[] = [
+        {
+            path: "/app/in/brands",
+            name: "Заказчики",
+            admin: true,
+        },
+        {
+            path: "/app/in/legals",
+            name: "Юр Лица",
+            admin: true,
+        },
+        {
+            path: "/app/in/sites",
+            name: "Объекты",
+            admin: true,
+        },
+        {
+            path: "/app/in/employees",
+            name: "Сотрудники",
+            admin: true,
+        },
+        {
+            path: "/app/in/import-sites",
+            name: "Импорт",
+            admin: true,
+        }
+    ].filter(adminFilter).map(mapFiltered);
+
+    const routes: TRoute[] = [
+        {
             path: "/app/in/dashboard",
             name: "Дашборд",
             icon: <BarChartOutlined />,
-        }] : [];
-
-    const adminRoutesRest = isAdmin ?
-        [
-            // {
-            //     path: "/app/in/subs",
-            //     name: "Подписки",
-            //     icon: <AntdIcons.AccountBookOutlined />,
-            // },
-            {
-                path: "/app/in/dicts",
-                name: "Справочники",
-                icon: <AppstoreOutlined />,
-                routes: [
-                    {
-                        path: "/app/in/brands",
-                        name: "Заказчики",
-                    },
-                    {
-                        path: "/app/in/legals",
-                        name: "Юр Лица",
-                    },
-                    {
-                        path: "/app/in/sites",
-                        name: "Объекты",
-                    },
-                    {
-                        path: "/app/in/employees",
-                        name: "Сотрудники",
-                    },
-                    {
-                        path: "/app/in/import-sites",
-                        name: "Импорт",
-                    }
-                ]
-            },
-            {
-                path: "/app/in/users",
-                name: "Пользователи",
-                icon: <Icons.UserOutlined/>
-            },
-            {
-                path: "/app/in/backup",
-                name: "Резервная копия  ",
-                icon: <Icons.HistoryOutlined/>
-            },
-        ] : [];
+            admin: true,
+        },
+        {
+            path: "/app/in/issues",
+            name: "Заявки",
+            icon: <MailOutlined />,
+        },
+        {
+            path: "/app/in/contracts",
+            name: "Договоры",
+            icon: <CalendarOutlined />,
+        },
+        {
+            path: "/app/in/dicts",
+            name: "Справочники",
+            icon: <AppstoreOutlined />,
+            routes: routesDictionaries,
+        },
+        {
+            path: "/app/in/users",
+            name: "Пользователи",
+            icon: <Icons.UserOutlined/>,
+            admin: true,
+        },
+        {
+            path: "/app/in/backup",
+            name: "Резервная копия  ",
+            icon: <Icons.HistoryOutlined/>,
+            admin: true,
+        }
+    ].filter(adminFilter).map(mapFiltered);
 
     return {
         route: {
-            routes: [
-                ...adminRoutesInit,
-                {
-                    path: "/app/in/issues",
-                    name: "Заявки",
-                    icon: <MailOutlined />,
-                },
-                {
-                    path: "/app/in/contracts",
-                    name: "Договоры",
-                    icon: <CalendarOutlined />,
-                },
-                ...adminRoutesRest
-            ],
+            routes,
         },
         appList: [],
     }
