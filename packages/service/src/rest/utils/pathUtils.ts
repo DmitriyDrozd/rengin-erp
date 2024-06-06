9649b553645d3e3f8dcadcaaa9de342f017a514d
dzmitryi.drozd <dzmitryi.drozd@gmail.com>
2024-06-06 18:42:31 +0300

    Перенос статики из проекта

--- packages/service/src/rest/utils/pathUtils.ts
+++ packages/service/src/rest/utils/pathUtils.ts
@@ -1,8 +1,19 @@
+import fs from 'node:fs';
 import { homedir } from 'os';
 import path from 'path';
 
 export const getStaticPath = () => {
-    // const userHomeDir = homedir();
-    // return path.join(userHomeDir, 'rengin-static');
-    return '/home/rengin-static';
+    let dir;
+    if (process.env.NODE_ENV === 'production') {
+        dir = '/home/rengin-static';
+    } else {
+        const userHomeDir = homedir();
+        dir = path.join(userHomeDir, 'rengin-static');
+    }
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, {recursive: true});
+    }
+
+    return dir;
 };
