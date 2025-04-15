268a42180099e2eebe4f0e338b67d09c5a6af615
dzmitryi.drozd <dzmitryi.drozd@gmail.com>
2025-04-15 11:38:05 +0300

    Исправлено резервное копирование после рестарта сервера

--- dump.sh
+++ dump.sh
@@ -7,6 +7,7 @@ mkdir -p $PROJECT_DEST
 FILE=$DEST/archive.gz
 echo $FILE
 
+docker exec mongodb sh -c "mkdir dump"
 docker exec mongodb sh -c "cd /dump & mkdir /dump/$DIR"
 docker exec mongodb sh -c "mongodump mongodb://rengin:BuildMeUp@rengindesk.ru:27017/rengin?authSource=admin --gzip --archive=/dump/$DIR/archive.gz"
 docker cp mongodb:/dump/$DIR/archive.gz $DEST/
